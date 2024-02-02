f83fd52b80f85052d8a536b2203a5150b1a453af
ilmi's <zulilmiihsn@gmail.com>
2024-02-02 11:50:39 +0800

    update view

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
   return (
     <main className="main-screen bg-[url('/bg.webp')]">
       <div className="relative main-screen grid">
-      <img src="fire.gif" alt=""/>
+        <img src="fire.gif" alt="" />
         <h1
           className={`absolute top-8 inset-x-0 text-center text-white text-2xl tracking-[.50em] ${garamond.className}`}
         >
@@ -17,7 +17,7 @@ export default function Home() {
         <img
           src="button.webp"
           alt=""
-          className="w-[8%] absolute left-2 -top-2 z-[2]"
+          className="w-[8%] absolute left-2 -top-2 z-[2] pointer"
         />
         <img
           src="goat.webp"
@@ -42,23 +42,23 @@ export default function Home() {
             className="w-[30%] absolute z-[1] justify-self-center -mt-4"
           />
         </div>
+        <div className="w-[40%] h-[55%] -right-4 -bottom-12 absolute pointer overflow-visible rounded-full">
         <img
           src="main-left.webp"
           alt=""
-          className="w-[50%] -right-4 bottom-0 absolute pointer"
+          className="overflow-visible object-cover h-[120%] -translate-y-32 -translate-x-4"
         />
-        <img
-          src="main-right.webp"
-          alt=""
-          className=" -left-32 bottom-24 absolute pointer"
-        />
+        </div>
+        <div className="w-[45%] rounded-full h-[30%] -left-2 bottom-64 absolute pointer overflow-visible">
+          <img src="main-right.webp" alt="" className="object-cover overflow-visible h-[160%]"/>
+        </div>
         <div className="w-[60%] h-[30%] absolute bottom-8 left-4 flex flex-col">
           <div className="h-1/5 flex gap-4 p-2">
-            <img src="position.webp" alt="" className="pointer z-[2]"/>
-            <img src="age.webp" alt="" className="pointer z-[2]"/>
+            <img src="position.webp" alt="" className="pointer z-[2]" />
+            <img src="age.webp" alt="" className="pointer z-[2]" />
           </div>
           <div className="h-4/5 -mt-6">
-            <img src="main-text.webp" alt="" className="pointer"/>
+            <img src="main-text.webp" alt="" className="pointer" />
           </div>
         </div>
       </div>
